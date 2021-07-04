a5b65cb4b9c04e748eda70e0dd26f09663fe7a3c
Nidrux <37250273+Nidrux@users.noreply.github.com>
2021-07-04 02:33:33 +0200

    updated to axios

--- javascript.js
+++ javascript.js
@@ -1,37 +1,13 @@
-/*
-  USING FETCH AND THEN.
-*/
-fetch('https://api.bypass.vip', {
-    method: 'POST',
-    body: new URLSearchParams({
-        'url': `${url}` //THE URL THAT YOU WANT TO BYPASS : string
-    })
-}).then(response => response.json())
-.then(data => {
-  console.log(data); //LOG THE RESPONSE TO THE CONSOLE.
-}).catch(err => {
-  console.log(err)
-});
-/*
-  IN A FUNCTION
-*/
-async function bypass() {
-    try {
-        const response = await fetch('https://api.bypass.vip', {
-            method: 'POST',
-            body: new URLSearchParams({
-                'url': `${url}` //THE URL THAT YOU WANT TO BYPASS : string
-            })
-        });
-        const result = await response.json()
-        if(result.success == true) {
-            console.log(result.destination)
-        }
-        else if (result.success == false) {
-            console.log(result.response)
-        }
-    }
-    catch {
-        console.log('Error contacting API')
-    }
-}
+const axios = require('axios');
+
+
+axios({
+  method: 'post',
+  url: 'https://api.bypass.vip/',
+  data: new URLSearchParams({
+    url: 'https://...' //URL to be bypassed
+  })
+}).then(response => {
+  let { data } = response;
+  console.log(data)
+}).catch(error => console.log(error))
