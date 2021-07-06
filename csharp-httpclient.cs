f01842357c561d13b890cec85a6862731d9e3064
directx5 <73019195+directx5@users.noreply.github.com>
2021-07-06 15:28:12 +0300

    Update csharp-httpclient.cs

--- csharp-httpclient.cs
+++ csharp-httpclient.cs
@@ -8,17 +8,12 @@ namespace BypassVIP_API
 {
     internal class Response
     {
-        public bool success { get; set; }
-
-        public string website { get; set; }
-
-        public string query { get; set; }
-
-        public string destination { get; set; }
-
-        public bool cache { get; set; }
-
-        public int time_ms { get; set; }
+        public bool Success { get; set; }
+        public string Website { get; set; }
+        public string Query { get; set; }
+        public string Destination { get; set; }
+        public bool Cache { get; set; }
+        public int TimeMs { get; set; }
     }
 
     public static class Program
@@ -27,18 +22,24 @@ namespace BypassVIP_API
 
         public static void Main(string[] args)
         {
-            Console.WriteLine(Post(args[0]).Result);
+            var result = Bypass("URL").Result;
+            
+            Console.WriteLine(result.Success);
+            Console.WriteLine(result.Website);
+            Console.WriteLine(result.Query);
+            Console.WriteLine(result.Destination);
+            Console.WriteLine(result.Cache);
+            Console.WriteLine(result.TimeMs);
         }
 
-        private static async Task<Response> Post(string url)
+        private static async Task<Response> Bypass(string url)
         {
             var values = new Dictionary<string, string>
             {
                 {"url", url}
             };
-
+            
             var response = await Client.PostAsync("https://api.bypass.vip/", new FormUrlEncodedContent(values));
-
             var responseString = await response.Content.ReadAsStringAsync();
 
             return JsonConvert.DeserializeObject<Response>(responseString);
