08c8ecacb1c66b19dd22f51e14914d63fbc1b591
purpl3r0se <purpl3r0se@protonmail.com>
2022-04-18 20:15:29 +0300

    Add files via upload

---
+++ golang.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+var (
+	ApiEndpoint = "https://api.bypass.vip/"
+)
+
+type BypassResponse struct {
+	Success   bool   `json:"success"`
+	Website   string `json:"website"`
+	Query     string `json:"query"`
+	TimeTaken int    `json:"time_ms"`
+
+	// Optional
+	Response    string `json:"response"`
+	Cache       string `json:"cache"`
+	Destination string `json:"destination"`
+}
+
+func Bypass(url string) BypassResponse {
+	var bResp BypassResponse
+	jsonStr := []byte(`{"url":"` + url + `"}`)
+	req, _ := http.NewRequest("POST", ApiEndpoint, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	err2 := json.Unmarshal(body, &bResp)
+
+	if err2 != nil {
+		fmt.Println(err)
+	}
+
+	return bResp
+}
